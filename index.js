960803c08fef2b600d6ceafd1868bda9a122b740
Travis Brackett <travmeister@gmail.com>
2021-02-23 22:16:19 -0800

    Use environment port

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+port = process.env.PORT || 8080
 
 const requestListener = function (req, res) {
   res.writeHead(200);
@@ -6,4 +7,4 @@ const requestListener = function (req, res) {
 }
 
 const server = http.createServer(requestListener);
-server.listen(8080);
+server.listen(port);
